8d15ad246d63cb11a6933cd3868f67979e85245e
Henrique dalla cort <henriquedallacort@hotmail.com>
2022-05-10 18:43:36 -0300

    Rotas narrativa/editar

--- routes/narrativa.ts
+++ routes/narrativa.ts
@@ -16,5 +16,19 @@ class NarrativaRoute {
 				item: null
 			});
 	}
+
+	public static async editar(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u)
+			res.redirect(app.root + "/acesso");
+		else
+			res.render("narrativa/editar", {
+				titulo: "Editar Narrativa",
+				textoSubmit: "Editar",
+				usuario: u,
+				item: null
+			});
+	}
+
 }
 export = NarrativaRoute;
