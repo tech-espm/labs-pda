9f0fa8da3731d22c5030d075103fd6d07e3ca0d0
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2022-05-27 17:35:35 -0300

    Ajuste dos estados

--- models/estado.ts
+++ models/estado.ts
@@ -18,6 +18,7 @@ interface Estado {
 }
 
 class Estado {
+	public static readonly CaminhaRelativoImagem = "public/img/estados/";
 	public static readonly TamanhoMaximoImagemEmBytes = 512 * 1024;
 
 	private static validar(estado: Estado): string {
@@ -25,59 +26,95 @@ class Estado {
 			return "Estado Inválido!";
 
 		estado.id = parseInt(estado.id as any);
-		
+
 		estado.idnarrativa = parseInt(estado.idnarrativa as any);
 		if (isNaN(estado.idnarrativa))
-				return "Narrativa inválida";
+			return "Narrativa inválida";
 
 		estado.titulo = (estado.titulo || "").normalize().trim();	
-			if (estado.titulo.length < 3 || estado.titulo.length > 30 )
-				return "Titulo inválido";
+		if (estado.titulo.length < 3 || estado.titulo.length > 30 )
+			return "Titulo inválido";
 
 		estado.descricao = (estado.descricao || "").normalize().trim();
-			if (estado.descricao.length < 3 || estado.descricao.length > 10000)
-				return "Descrição inválida";
+		if (estado.descricao.length < 3 || estado.descricao.length > 10000)
+			return "Descrição inválida";
 
-			
-		estado.idestado1 = parseInt(estado.idestado1 as any);
-		if (isNaN)
+		let ids = [
+			parseInt(estado.idestado1 as any),
+			parseInt(estado.idestado2 as any),
+			parseInt(estado.idestado3 as any),
+			parseInt(estado.idestado4 as any),
+			parseInt(estado.idestado5 as any)
+		];
 
-		estado.texto1 = (estado.texto1 || "").normalize().trim();	
-			if (estado.texto1.length < 3 || estado.texto1.length > 50 )
-				return "texto inválido";
+		let textos = [
+			(estado.texto1 || "").normalize().trim(),
+			(estado.texto2 || "").normalize().trim(),
+			(estado.texto3 || "").normalize().trim(),
+			(estado.texto4 || "").normalize().trim(),
+			(estado.texto5 || "").normalize().trim()
+		];
 
-		//DUVIDA PARA O RAFA ID TEXTO 		
-		let contador = 1;
-		estado.idestado2 = parseInt(estado.idestado2 as any);
-		estado.idestado3 = parseInt(estado.idestado3 as any);
-		estado.idestado4 = parseInt(estado.idestado4 as any);
-		estado.idestado5 = parseInt(estado.idestado5 as any);
+		let preenchidos = 0;
 
+		for (let i = textos.length - 1; i >= 0; i--) {
+			if (!ids[i] && !textos[i]) {
+				if (preenchidos || !i)
+					return "Opção " + (i + 1) + " vazia";
 
-		return null;	
+				ids[i] = 0;
+				textos[i] = null;
+				continue;
+			}
+
+			if (isNaN(ids[i]))
+				return "Opção " + (i + 1) + " inválida";
+
+			if (!textos[i]) {
+				// Se só tiver a opção 1 (índice 0), o texto é opcional
+				if (!preenchidos && !i)
+					textos[i] = null;
+				else
+					return "Texto da opção " + (i + 1) + " inválido";
+			}
+
+			if (textos[i].length > 100)
+				return "Texto da opção " + (i + 1) + " inválido";
+
+			preenchidos++;
+		}
+
+		estado.idestado1 = ids[0];
+		estado.idestado2 = ids[1];
+		estado.idestado3 = ids[2];
+		estado.idestado4 = ids[3];
+		estado.idestado5 = ids[4];
+
+		estado.texto1 = textos[0];
+		estado.texto2 = textos[1];
+		estado.texto3 = textos[2];
+		estado.texto4 = textos[3];
+		estado.texto5 = textos[4];
+
+		return null;
 	}
 
 	public static listar(idnarrativa: number, idusuario: number, admin: boolean): Promise<Estado[]> {
-
 		return app.sql.connect(async (sql) => {
-			if(admin){
-				return (await sql.query ("select n.id, n.idusuario, n.nome, e.id, e.titulo, e.idestado1, e.texto1, e.idestado2, e.texto2 , e.idestado3, e.texto3,  e.idestado4, e.texto4 , e.idestado5, e.texto5  from narrativa n inner join estado e on n.idnarrativa=e.idnarrativa;")) as Estado [] || [];
-			}else{
-				return (await sql.query ("select id, titulo, descricao , idestado1, texto1, idestado2, texto2 , idestado3, texto3, idestado4, texto4 ,idestado5, texto5 from estado where idnarrativa = ?", [idnarrativa])) as Estado [] || [];
-			}
-			
+			if (admin)
+				return (await sql.query("select id, titulo, descricao, idestado1, texto1, idestado2, texto2, idestado3, texto3, idestado4, texto4, idestado5, texto5 from estado where idnarrativa = ?", [idnarrativa])) as Estado[] || [];
+			else
+				return (await sql.query("select e.id, e.titulo, e.idestado1, e.texto1, e.idestado2, e.texto2, e.idestado3, e.texto3, e.idestado4, e.texto4, e.idestado5, e.texto5 from narrativa n inner join estado e on e.idnarrativa = n.id where n.id = ? and n.idusuario = ?", [idnarrativa, idusuario])) as Estado[] || [];
 		});
 	}
 
 	public static obter(id: number, idnarrativa: number, idusuario: number, admin: boolean): Promise<Estado> {
-		
 		return app.sql.connect(async (sql) => {
 			let lista : Estado[];
-				if(admin){
-					lista = (await sql.query ("select id , titulo , descricao ,  idestado1, texto1, idestado2, texto2 , idestado3, texto3, idestado4, texto4 ,idestado5, texto5 from estado where id = ?", [id])) as Estado [];
-				}else{
-					lista = (await sql.query ("select id , titulo, descricao ,  idestado1, texto1, idestado2, texto2 , idestado3, texto3, idestado4, texto4 ,idestado5, texto5 from estado where id = ? and idusuario = ?", [id,idusuario])) as Estado []; 
-				}
+			if (admin)
+				lista = (await sql.query("select id, titulo, descricao, idestado1, texto1, idestado2, texto2, idestado3, texto3, idestado4, texto4, idestado5, texto5 from estado where id = ? and idnarrativa = ?", [id, idnarrativa])) as Estado[] || [];
+			else
+				lista = (await sql.query("select e.id, e.titulo, e.idestado1, e.texto1, e.idestado2, e.texto2, e.idestado3, e.texto3, e.idestado4, e.texto4, e.idestado5, e.texto5 from narrativa n inner join estado e on e.idnarrativa = n.id where e.id = ? and n.id = ? and n.idusuario = ?", [id, idnarrativa, idusuario])) as Estado[] || [];
 			return (lista && lista[0]) || null;
 		});
 	}
@@ -94,9 +131,21 @@ class Estado {
 			return Promise.resolve("A imagem deve ter, no máximo, " + Estado.TamanhoMaximoImagemEmBytes + " bytes");
 
 		return app.sql.connect(async (sql) => {
+			if (!admin) {
+				const i = await sql.scalar("select id from narrativa where id = ? and idusuario = ?", [estado.idnarrativa, idusuario]);
+				if (!i)
+					return "Narrativa não encontrada";
+			}
 
-			await app.fileSystem.saveUploadedFile("public/xxx/yyy/zzz.jpg", imagem);
-			await sql.query ("insert into estado (idnarrativa, titulo, descricao, idestado1, texto1, idestado2, texto2, idestado3, texto3, idestado4, texto4, idestado5, texto5) values (?,?,?,?,?,?,?,?,?,?,?,?,?)" , [estado.idnarrativa , estado.titulo, estado.descricao, estado.idestado1, estado.texto1,estado.idestado2, estado.texto2, estado.idestado3, estado.texto3,estado.idestado4,estado.texto4, estado.idestado5, estado.texto5]);
+			await sql.beginTransaction();
+
+			await sql.query("insert into estado (idnarrativa, titulo, descricao, idestado1, texto1, idestado2, texto2, idestado3, texto3, idestado4, texto4, idestado5, texto5) values (?,?,?,?,?,?,?,?,?,?,?,?,?)" , [estado.idnarrativa , estado.titulo, estado.descricao, estado.idestado1, estado.texto1,estado.idestado2, estado.texto2, estado.idestado3, estado.texto3,estado.idestado4,estado.texto4, estado.idestado5, estado.texto5]);
+
+			estado.id = await sql.scalar("select last_insert_id()");
+
+			await app.fileSystem.saveUploadedFile(Estado.CaminhaRelativoImagem + estado.id + ".jpg", imagem);
+
+			await sql.commit();
 
 			return null;
 		});
@@ -111,33 +160,44 @@ class Estado {
 			return Promise.resolve("A imagem deve ter, no máximo, " + Estado.TamanhoMaximoImagemEmBytes + " bytes");
 
 		return app.sql.connect(async (sql) => {
-
-			if (imagem){
-				await app.fileSystem.saveUploadedFile("public/xxx/yyy/zzz.jpg", imagem);
+			if (!admin) {
+				const i = await sql.scalar("select id from narrativa where id = ? and idusuario = ?", [estado.idnarrativa, idusuario]);
+				if (!i)
+					return "Narrativa não encontrada";
 			}
 
-			await sql.query ("update estado set titulo = ?, descricao = ?, texto1 = ?, texto2 = ?, texto3 = ?, texto4 = ?, texto5 = ? where id = ? , idusuario = ?)" , [ estado.titulo, estado.descricao, estado.texto1, estado.texto2,  estado.texto3, estado.texto4, estado.texto5, estado.id, idusuario]);
-	
+			await sql.beginTransaction();
+
+			await sql.query("update estado set titulo = ?, descricao = ?, idestado1 = ?, texto1 = ?, idestado2 = ?, texto2 = ?, idestado3 = ?, texto3 = ?, idestado4 = ?, texto4 = ?, idestado5 = ?, texto5 = ? where id = ? and idnarrativa = ?" , [estado.titulo, estado.descricao, estado.idestado1, estado.texto1, estado.idestado2, estado.texto2, estado.idestado3, estado.texto3, estado.idestado4, estado.texto4, estado.idestado5, estado.texto5, estado.id, estado.idnarrativa]);
+
+			if (!sql.affectedRows)
+				return "Opção não encontrada";
+
+			if (imagem)
+				await app.fileSystem.saveUploadedFile(Estado.CaminhaRelativoImagem + estado.id + ".jpg", imagem);
+
+			await sql.commit();
+
 			return null;
 		});
 	}
 
 	public static excluir(id: number, idnarrativa: number, idusuario: number, admin: boolean): Promise<string> {
 		return app.sql.connect(async (sql) => {
+			if (!admin) {
+				const i = await sql.scalar("select id from narrativa where id = ? and idusuario = ?", [idnarrativa, idusuario]);
+				if (!i)
+					return "Narrativa não encontrada";
+			}
 
 			await sql.beginTransaction();
 
-			let idestado = await sql.query ("select id from estado where id = ?" , [id]);
-
-			if (admin)
-				await sql.query("delete from estado where id = ?", [id]);
-			else
-				await sql.query("delete from estado where id = ? and idusuario = ?", [id, idusuario]);
+			await sql.query("delete from estado where id = ? and idnarrativa = ?", [id, idnarrativa]);
 
 			if (!sql.affectedRows)
-				return "Estado não encontrado";
+				return "Opção não encontrada";
 
-			//EXCLUIR IMAGENS DO ESTADO 
+			//EXCLUIR IMAGENS DO ESTADO
 
 			await sql.commit();
 
